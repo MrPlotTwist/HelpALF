9c95fb3f8146cc25edb0b516cd4c0dac268180df
tomi <tomi2002@gmx.at>
2024-05-06 21:14:58 +0200

    mehr Testfälle wurden geadded

--- Help ALF/spec/string_calculator_spec.js
+++ Help ALF/spec/string_calculator_spec.js
@@ -7,6 +7,18 @@ describe('Help ALF', function() {
 		expect(calculator.add('')).toEqual("Spaceship lost forever.");
 	});
 
+	it('should return Spaceship lost forever. for 2 string', function() {
+		expect(calculator.add('2')).toEqual('Spaceship lost forever.');
+	});
+
+	it('should return Spaceship lost forever. for 2,4 string', function() {
+		expect(calculator.add('2,4')).toEqual('Spaceship lost forever.');
+	});
+
+	it('should return Spaceship lost forever. for abc,def string', function() {
+		expect(calculator.add('abc,def')).toEqual('Spaceship lost forever.');
+	});
+
 	it('should return Spaceship found! for 7,2 string', function() {
 		expect(calculator.add('7,2')).toEqual('Spaceship found!');
 	});
